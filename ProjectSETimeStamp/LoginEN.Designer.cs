8b19aa813dc5add7dc7bd4bba19c59e848c62079
KROLARP THIRATANABOON <s6104022630017@kmutnb.ac.th>
2021-11-13 20:50:41 +0700

    Login Form

---
+++ ProjectSETimeStamp/LoginEN.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace ProjectSETimeStamp
+{
+    partial class LoginEN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Username_TB = new System.Windows.Forms.TextBox();
+            this.Password_TB = new System.Windows.Forms.TextBox();
+            this.Username_LB = new System.Windows.Forms.Label();
+            this.Password_LB = new System.Windows.Forms.Label();
+            this.Submit_BT = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Username_TB
+            // 
+            this.Username_TB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Username_TB.Location = new System.Drawing.Point(193, 3);
+            this.Username_TB.Name = "Username_TB";
+            this.Username_TB.Size = new System.Drawing.Size(438, 22);
+            this.Username_TB.TabIndex = 1;
+            this.Username_TB.UseWaitCursor = true;
+            this.Username_TB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Username_TB_KeyPress);
+            // 
+            // Password_TB
+            // 
+            this.Password_TB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Password_TB.Location = new System.Drawing.Point(193, 67);
+            this.Password_TB.Name = "Password_TB";
+            this.Password_TB.Size = new System.Drawing.Size(438, 22);
+            this.Password_TB.TabIndex = 2;
+            this.Password_TB.UseWaitCursor = true;
+            this.Password_TB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Password_TB_KeyPress);
+            // 
+            // Username_LB
+            // 
+            this.Username_LB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Username_LB.AutoSize = true;
+            this.Username_LB.Location = new System.Drawing.Point(3, 0);
+            this.Username_LB.Name = "Username_LB";
+            this.Username_LB.Size = new System.Drawing.Size(184, 64);
+            this.Username_LB.TabIndex = 3;
+            this.Username_LB.Text = "USERNAME : ";
+            this.Username_LB.UseWaitCursor = true;
+            // 
+            // Password_LB
+            // 
+            this.Password_LB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Password_LB.AutoSize = true;
+            this.Password_LB.Location = new System.Drawing.Point(3, 64);
+            this.Password_LB.Name = "Password_LB";
+            this.Password_LB.Size = new System.Drawing.Size(184, 65);
+            this.Password_LB.TabIndex = 4;
+            this.Password_LB.Text = "PASSWORD : ";
+            this.Password_LB.UseWaitCursor = true;
+            // 
+            // Submit_BT
+            // 
+            this.Submit_BT.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Submit_BT.AutoSize = true;
+            this.Submit_BT.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Submit_BT.Location = new System.Drawing.Point(256, 3);
+            this.Submit_BT.Name = "Submit_BT";
+            this.Submit_BT.Size = new System.Drawing.Size(120, 36);
+            this.Submit_BT.TabIndex = 6;
+            this.Submit_BT.Text = "เข้าสู่ระบบ";
+            this.Submit_BT.UseVisualStyleBackColor = true;
+            this.Submit_BT.UseWaitCursor = true;
+            this.Submit_BT.Click += new System.EventHandler(this.Submit_BT_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel2.Controls.Add(this.Username_LB, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.Password_LB, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.Password_TB, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.Username_TB, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(83, 228);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(634, 129);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(83, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(634, 219);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel3.Controls.Add(this.Submit_BT, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(83, 363);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(634, 84);
+            this.tableLayoutPanel3.TabIndex = 8;
+            // 
+            // LoginEN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "LoginEN";
+            this.Text = "LoginEN";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Username_TB;
+        private System.Windows.Forms.TextBox Password_TB;
+        private System.Windows.Forms.Label Username_LB;
+        private System.Windows.Forms.Label Password_LB;
+        private System.Windows.Forms.Button Submit_BT;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+    }
+}
---
+++ ProjectSETimeStamp/LoginEN.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProjectSETimeStamp
+{
+    public partial class LoginEN : Form
+    {
+        public LoginEN()
+        {
+            InitializeComponent();
+            
+        }
+        //Connect SQL Server
+        //string con = "Data Source =.;Initial Catalog = shop;User ID = sa;Password = 123456;";
+        //"Data Source=$Local;Initial Catalog=$Database;User ID=$USer;Password=$Password"
+        /*private string ConnectAndQuery(String ConnectSQL,String QuerySQL)
+        {
+            //String connectionstring = null;
+            DataTable SQLQuery_DT = new DataTable();
+            SqlConnection cnn;
+            cnn = new SqlConnection(ConnectSQL);
+            SqlCommand cmd = new SqlCommand(QuerySQL, cnn);
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("สามารถConnectDatabase ได้");
+                SqlDataAdapter Send_SQL_to_DB = new SqlDataAdapter(cmd);
+                Send_SQL_to_DB.Fill(SQLQuery_DT);
+                cnn.Close();
+                Send_SQL_to_DB.Dispose();
+            }
+            catch
+            {
+                MessageBox.Show("ไม่สามารถเข้าถึง Database ได้");
+            }
+            return  ;
+        }
+       */
+        private void Submit_BT_Click(object sender, EventArgs e)
+        {
+            //String SQL_Query = "";
+            //string con = "Data Source=$Local;Initial Catalog=$Database;User ID=$USer;Password=$Password";
+            String Username = Username_TB.Text;
+            String Password = Password_TB.Text;
+            // Submit ผ่านไปหน้า form1
+            Form1 homeform = new Form1() ;
+            if (Username == "js" && Password == "va")
+            {
+                homeform.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("USERNAME หรือ PASSWORD ไม่ถูกต้อง","Warning input is Fail",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void Username_TB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((int)e.KeyChar >= 65 && (int)e.KeyChar <= 122) || ((int)e.KeyChar >= 48 && (int)e.KeyChar <= 57) || (int)e.KeyChar == 8 || (int)e.KeyChar == 13 || (int)e.KeyChar == 46)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+                
+                //MessageBox.Show("กรุณาระบุข้อมุลตัวอังกฤษ", "Username ผิดพลาด", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Password_TB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((int)e.KeyChar >= 65 && (int)e.KeyChar <= 122) || ((int)e.KeyChar >= 48 && (int)e.KeyChar <= 57) || (int)e.KeyChar == 8 || (int)e.KeyChar == 13 || (int)e.KeyChar == 46)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+
+                //MessageBox.Show("กรุณาระบุข้อมุลตัวอังกฤษ", "Username ผิดพลาด", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
